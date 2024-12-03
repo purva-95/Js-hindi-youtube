11f96494db07343cb7a15cc9dfbe2cc9ed3ffe2e
purva-95 <purvagaikwad16@gmail.com>
2024-12-03 16:26:05 +0000

    datatype summary

---
+++ datatype-summary.js
@@ -0,0 +1,11 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail ;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+
+console.log(hello);
