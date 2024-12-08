06afc82f67042f0f8a3e1e8a583a9473dc13d1fd
purva-95 <purvagaikwad16@gmail.com>
2024-12-08 11:30:39 +0000

    dates

--- 06-dates.js
+++ 06-dates.js
@@ -13,5 +13,20 @@
 //let myCreatedDate = new Date("2024-01-14")
 //console.log(myCreatedDate.toLocaleString());
 
-//let myCreatedDate = new Date("01-14-2024")
+let myCreatedDate = new Date("01-14-2024")
 //console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() +1);
+console.log(newDate.getDay());
+//`${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+})
