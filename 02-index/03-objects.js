82853519f2b66149db093e78c641a953e74c82da
purva-95 <purvagaikwad16@gmail.com>
2024-12-10 13:24:47 +0000

    object

--- 02-index/03-objects.js
+++ 02-index/03-objects.js
@@ -1,5 +1,5 @@
 // objects literals
-const mySym = symbol("key1")
+const mySym = Symbol("key1")
 
 const JsUser = {
     name: "purva",
@@ -12,7 +12,23 @@ const JsUser = {
     lastLoginDays: ["monday", "Saturday"]
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
-console.log(JsUser[mySym])
+//console.log(JsUser.email)
+//console.log(JsUser["email"])
+//console.log(JsUser["full name"])
+//console.log(JsUser[mySym])
+
+
+JsUser.email = "purva@gmail.com"
+//Object.freeze(JsUser)
+JsUser.email = "purva1@gmail.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
