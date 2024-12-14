bb6c94fbec3d331a3afdd82b326d697a65efb15c
purva-95 <purvagaikwad16@gmail.com>
2024-12-14 10:36:13 +0000

    object

--- 02-index/03-objects.js
+++ 02-index/03-objects.js
@@ -24,11 +24,67 @@ JsUser.email = "purva1@gmail.com"
 //console.log(JsUser);
 
 JsUser.greeting = function(){
-    console.log("Hello JS user");
+    //console.log("Hello JS user");
 }
 
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    //console.log(`Hello JS user, ${this.name}`);
 }
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+//console.log(JsUser.greeting());
+//console.log(JsUser.greetingTwo());
+
+
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email:"some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "purva",
+            lastname: "shinde"
+        }
+
+    }
+}
+
+//console.log(regularUser.fullname);
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+//const obj3 = { obj1, obj2}
+//const obj3 = Object.assign({} ,obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const users =[
+   
+    {
+        id: 1,
+        email: "h@gmail.com" 
+     }, 
+     {
+        id: 1,
+        email: "h@gmail.com" 
+     }, 
+     {
+        id: 1,
+        email: "h@gmail.com" 
+     },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
