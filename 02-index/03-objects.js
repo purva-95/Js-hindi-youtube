5156cf68157947c6bfd7b964c4c2a46957676540
purva-95 <purvagaikwad16@gmail.com>
2024-12-16 10:21:10 +0000

    json

--- 02-index/03-objects.js
+++ 02-index/03-objects.js
@@ -81,10 +81,31 @@ const users =[
 ]
 
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "purva"
+}
+
+const {courseInstructor} = course
+
+//console.log(courseInstructor);
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+// JSON API
+//{
+//  "name": "purva",
+//  "coursename": "js in hindi",
+//  "price": "free"
+//}
