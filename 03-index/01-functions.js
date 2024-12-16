77b91628b2fe38c89ab1b10dde673932048378c2
purva-95 <purvagaikwad16@gmail.com>
2024-12-16 11:52:16 +0000

    function objects

--- 03-index/01-functions.js
+++ 03-index/01-functions.js
@@ -20,9 +20,44 @@ function loginUserMessage(username){
 
 function loginUserMessage(username ="sam"){
     if(!username){
-        console.log("please enter a username");
+       // console.log("please enter a username");
         return
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("purva"))
+//console.log(loginUserMessage("purva"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500, 2000))
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "purva",
+    price: 199
+}
+
+function handleObject(anyobject){
+    //console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([500, 200, 400, 1000]));
