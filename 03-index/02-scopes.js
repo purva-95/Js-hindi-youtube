3c0c4255aa064584c7e143f457d365873218dba4
purva-95 <purvagaikwad16@gmail.com>
2024-12-17 16:36:58 +0000

    hoisting

--- 03-index/02-scopes.js
+++ 03-index/02-scopes.js
@@ -1,8 +1,41 @@
 let a = 300
 if (true) {
     let a = 10
-    const b = 0
-    console.log("INNER: ",a);
+    const b = 20
+   // console.log("INNER: ",a);
 }
 
-console.log(a);
+//console.log(a);
+
+function one(){
+    const username = "purva"
+
+    function two(){
+        const website = "youtube"
+       // console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true) {
+    const username = "purva"
+    if(username === "purva") {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+   // console.log(website);
+}
+
+//console.log(username);
+
+console.log(addone(5))
+function  addone(num){
+    return num + 1
+}
+
+//addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
